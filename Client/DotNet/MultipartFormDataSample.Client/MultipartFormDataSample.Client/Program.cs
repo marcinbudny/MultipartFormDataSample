e51a9a24837a65ea67e4be2c290466b2e8b95978
Marcin Budny <marcin.budny@gmail.com>
2014-02-21 08:40:09 +0100

    introduced web api implementation

--- Client/DotNet/MultipartFormDataSample.Client/MultipartFormDataSample.Client/Program.cs
+++ Client/DotNet/MultipartFormDataSample.Client/MultipartFormDataSample.Client/Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -18,7 +20,9 @@ namespace MultipartFormDataSample.Client
 
             foreach (var path in Directory.EnumerateFiles("../../../../../SampleImages"))
             {
-                content.Add(new StreamContent(File.Open(path, FileMode.Open)), "files", Path.GetFileName(path));                
+                var imageContent = new StreamContent(File.Open(path, FileMode.Open));
+                imageContent.Headers.ContentType = GetMediaTypeFromFilePath(path);
+                content.Add(imageContent, "files", Path.GetFileName(path));                
             }
 
             var model = new SampleModel
@@ -35,9 +39,26 @@ namespace MultipartFormDataSample.Client
             content.Add(new StringContent(json, Encoding.UTF8, "application/json"), "model");
 
             var client = new HttpClient();
-            var result = client.PostAsync("http://localhost:53908/Home/SendWithJson", content);
+            var result = client.PostAsync("http://localhost:53908/api/send", content);
+            //var result = client.PostAsync("http://localhost:53908/Home/SendWithJson", content);
             
             Debug.WriteLine(result.Result.ToString());
+        }
+
+        private static MediaTypeHeaderValue GetMediaTypeFromFilePath(string path)
+        {
+            switch (Path.GetExtension(path))
+            {
+                case ".jpg":
+                case ".jpeg":
+                    return new MediaTypeHeaderValue("image/jpeg");
+
+                case ".png":
+                    return new MediaTypeHeaderValue("image/png");
+
+                default:
+                    return new MediaTypeHeaderValue("application/octet-stream");
+            }
         }
     }
 }
---
+++ Server/MultipartFormDataSample.Server/MultipartFormDataSample.Server/Controllers/SendApiController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using MultipartFormDataSample.Server.Models;
+
+namespace MultipartFormDataSample.Server.Controllers
+{
+    public class SendApiController : ApiController
+    {
+        [Route("api/send")]
+        public async Task<HttpResponseMessage> SendWithJson()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var provider = await Request.Content.ReadAsMultipartAsync();
+
+            var modelContent = provider.Contents
+                .FirstOrDefault(c => c.Headers.ContentDisposition.Name == "model");
+            var model = await modelContent.ReadAsAsync<SampleModel>();
+
+            var fileContents = provider.Contents
+                .Where(c => c.Headers.ContentDisposition.Name == "files")
+                .ToList();
+
+            fileContents.ForEach(async fc =>
+            {
+                var fileBytes = await fc.ReadAsByteArrayAsync();
+                var fileType = fc.Headers.ContentType.MediaType;
+                var fileName = fc.Headers.ContentDisposition.FileName;
+
+                Trace.WriteLine(string.Format("Got image {0} of type {1} and size {2} bytes", fileName, fileType, fileBytes.Length));
+            });
+            
+            return Request.CreateResponse(HttpStatusCode.OK);
+
+        }
+    }
+}
