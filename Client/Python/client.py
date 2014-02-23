79bded8a7c4f342ccd1729259608748bc27c4ed0
Marcin Budny <marcin.budny@gmail.com>
2014-02-23 23:18:46 +0100

    started working on python client

---
+++ Client/Python/client.py
@@ -0,0 +1,31 @@
+import urllib2
+import MultipartPostHandler
+import json
+import os
+import cookielib
+
+def send_image_set (image_set, images):
+	
+	image_set_json = json.dumps(image_set)
+	multipart_data = dict({"imageset" : image_set_json}.items() + images.items())
+	print multipart_data
+	
+	cookies = cookielib.CookieJar()
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies), MultipartPostHandler.MultipartPostHandler)
+	opener.open("http://localhost:53908/api/send", multipart_data)
+	
+
+image_set = {
+				"name" : "Image Set"
+			}
+			
+images = {}
+counter = 0
+for dirpath, dnames, fnames in os.walk("../SampleImages"):
+    for file in fnames:
+		if file.endswith(".jpg") or file.endswith(".png"):
+			images["image%d" % counter] = open(os.path.join(dirpath, file), "rb")
+			counter += 1
+
+send_image_set(image_set, images)
+
